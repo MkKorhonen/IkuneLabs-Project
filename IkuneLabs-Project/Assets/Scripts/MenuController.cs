d67c91ddb269ec085366db31e7b67f337e80698f
JoniKilpe <joni.kilpe@gmail.com>
2022-03-30 15:38:42 +0300

    Added Menu/Settings U.I and functions
    Patch notes: I left gameplay settings out from this version as we haven't discussed if there's going to be any (Just throwing out there, for example, language options / hints / etc...). At the moment there are: Settings-icon in the top-right of the gameplay screen. When pressed it opens the menu. Menu has Resume (resumes game/closes menu), Settings (leads to sound settings and back-button) and Exit, which closes the game (should close at least, I haven't tested). Sound settings has Volume slider, which has general volume for the whole game (AudioListener), Reset to default(changes volume to hardcoded default which is 0.5. Range 0.0 - 1.0) and Apply & Back buttons. Apply remembers the value the user chose or at least should remember. When we have soundFX ready, I can apply them to the menu so we can have some menu sounds.

---
+++ IkuneLabs-Project/Assets/Scripts/MenuController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuController : MonoBehaviour
+{
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 0.5f;
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+    public void ExitButton() {
+        Application.Quit();
+    }
+
+    public void setVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string MenuType) {
+        if(MenuType == "Audio") {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(1);
+        confirmationPrompt.SetActive(false);
+    }
+}
+
